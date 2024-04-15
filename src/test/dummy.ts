d646bebb56574adb97a1721b14f43a9113bccde9
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-04-15 19:20:25 +0900

    Refactor GameCard stories to use dummyGame data

---
+++ src/test/dummy.ts
@@ -0,0 +1,14 @@
+import { PlayedGameData } from '../types/Game';
+
+export const dummyGame: PlayedGameData = {
+  appid: '412830',
+  name: 'STEINS;GATE',
+  playtime_forever: 2364,
+  img_icon_url: 'https://cdn.cloudflare.steamstatic.com/steam/apps/412830/capsule_231x87.jpg',
+  has_community_visible_stats: true,
+  playtime_windows_forever: 0,
+  playtime_mac_forever: 0,
+  playtime_linux_forever: 0,
+  has_leaderboards: false,
+  rtime_last_played: 1610000000,
+};
