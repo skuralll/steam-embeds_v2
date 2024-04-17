879e49a8b16511f64fae77b27898174bf3e7c08b
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-04-17 13:37:53 +0900

    Implement showing widget

--- src/lib/steam.ts
+++ src/lib/steam.ts
@@ -1,3 +1,36 @@
+import { PlayedGameData } from '../types/Game';
+import { PlayerSummary } from '../types/Player';
+
+export async function getPlayerSummary(steamid: string): Promise<PlayerSummary | null> {
+  try {
+    const data = await fetch(`/api/player?steamid=${steamid}`);
+    const json = await data.json();
+    // レスポンス検証
+    if (json.response['players'].length === 0) {
+      console.log('❗️Error on fetching player summary. SteamID may be incorrect');
+      return null;
+    }
+    // うまく取得できていれば返す
+    return json.response.players[0] as PlayerSummary;
+  } catch (e) {
+    console.log('❗️Error on fetching player summary');
+    console.error(e);
+    return null;
+  }
+}
+
+export async function getOwnedGames(steamid: string): Promise<PlayedGameData[]> {
+  try {
+    const data = await fetch(`/api/games?steamid=${steamid}`);
+    const json = await data.json();
+    return json.response.games as PlayedGameData[];
+  } catch (e) {
+    console.log('❗️Error on fetching owned games');
+    console.error(e);
+    return [];
+  }
+}
+
 export function getGameHeaderURL(appid: string): string {
   return `https://cdn.akamai.steamstatic.com/steam/apps/${appid}/header.jpg`;
 }
--- src/pages/WidgetPage.tsx
+++ src/pages/WidgetPage.tsx
@@ -1,5 +1,44 @@
-const HomePage = () => {
-  return <div>This is Widget</div>;
+import { useLocation } from 'react-router-dom';
+import { PlayerSummary } from '../types/Player';
+import { useEffect, useState } from 'react';
+import { getOwnedGames, getPlayerSummary } from '../lib/steam';
+import { PlayedGameData } from '../types/Game';
+import Widget from '../components/Widget';
+
+const DEFAULT_NUM = 8;
+
+const WidgetPage = () => {
+  // State
+  const [player, setPlayer] = useState<PlayerSummary | null>(null);
+  const [games, setGames] = useState<PlayedGameData[] | null>(null);
+  useEffect(() => {
+    async function fetchData() {
+      setPlayer(await getPlayerSummary(steamid!));
+      setGames(
+        (await getOwnedGames(steamid!))
+          .sort((a, b) => b.playtime_forever - a.playtime_forever)
+          .slice(0, num)
+      );
+    }
+    fetchData();
+  }, []);
+  // query params
+  const search = useLocation().search;
+  const query = new URLSearchParams(search);
+  const steamid = query.get('id');
+  if (steamid === null) {
+    return <div className="px-2 py-1">Invalid Steam ID</div>;
+  }
+  const num = Number(query.get('num') ?? DEFAULT_NUM);
+  // Render
+  if (player && games) {
+    // success to get data
+    return <Widget title={'Most Played Games'} player={player} games={games} />;
+  } else {
+    // failed to get data
+    // TODO show loading screen
+    return <div>This is null</div>;
+  }
 };
 
-export default HomePage;
+export default WidgetPage;
