f1cf79cde4906b3fedaad81ae8d29fb051e83be4
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-02 14:50:00 +0900

    Changed header design

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -3,8 +3,8 @@ import logo from '../assets/embedslogo.svg';
 const AppLogo = () => {
   return (
     <div className="flex items-center space-x-2">
-      <img src={logo} width={36} height={36} alt="logo" />
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+      <img src={logo} width={32} height={32} alt="logo" />
+      <span className="self-center text-xl font-semibold whitespace-nowrap text-[#c5c3c0]">
         SteamEmbeds
       </span>
     </div>
--- src/features/home/HomeHeader.tsx
+++ src/features/home/HomeHeader.tsx
@@ -3,7 +3,7 @@ import AppLogo from '../../components/AppLogo';
 const HomeHeader = () => {
   return (
     <nav className="bg-[#181d24] shadow">
-      <div className="h-20 flex flex-wrap items-center justify-between p-4">
+      <div className="h-12 flex flex-wrap items-center justify-between px-4">
         <AppLogo />
       </div>
     </nav>
