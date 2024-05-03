a22025657a550d5fe013b9e2bd098e15d946d217
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-03 15:30:13 +0900

    Add WhatIsThis component to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,15 @@
+import WhatIsThis from '../features/home/ WhatIsThis';
 import HomeHeader from '../features/home/HomeHeader';
 
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-[#1e2837]">
       <HomeHeader />
+      <div className="flex flex-col items-center">
+        <div className="max-w-3xl bg-[rgb(26,35,48)] px-3 py-2 w-full">
+          <WhatIsThis />
+        </div>
+      </div>
     </div>
   );
 };
