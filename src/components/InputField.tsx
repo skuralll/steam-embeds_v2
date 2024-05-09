00a1e084f22bf5dc7469ded108f3e60cad5f8725
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-10 08:04:07 +0900

    Refactor InputField component props destructuring

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,20 +1,21 @@
+import { ComponentProps } from 'react';
+
 type Props = {
   label: string;
   placeholder: string;
   value: string;
   setValue: React.Dispatch<React.SetStateAction<string>>;
-};
+} & ComponentProps<'input'>;
 
-const InputField = ({ label, placeholder, value, setValue }: Props) => {
+const InputField = ({ label, value, setValue, ...props }: Props) => {
   return (
     <div>
       <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
       <input
-        type="text"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        placeholder={placeholder}
         value={value}
         onChange={(e) => setValue(e.target.value)}
+        {...props}
       />
     </div>
   );
