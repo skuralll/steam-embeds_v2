4475c72e0de84bcf2a491c8575f193d732ec733f
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-10 07:17:16 +0900

    Remove unnecessary comments and improve code readability

--- src/features/home/widget_generator/ShowWidgetModal.tsx
+++ src/features/home/widget_generator/ShowWidgetModal.tsx
@@ -16,24 +16,19 @@ const ShowWidgetModal = ({ isOpen, setIsOpen, params }: Props) => {
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
       <div className="flex flex-col gap-[15px] max-w-2xl">
-        {/* ヘッダ */}
         <div className="flex items-center gap-2">
           <h1 className="text-[22px]">埋め込みコード生成完了</h1>
           <div className="w-[22px] h-[22px]">
             <CheckMark />
           </div>
         </div>
-        {/* 説明 */}
         <span>
           埋め込みコードを生成しました。以下のコードをコピーして、あなたのサイトに貼り付けてください。サイズや表示内容を調整したい場合は、このモーダルを閉じて再度パラメータを設定してください。
         </span>
-        {/* セパレータ */}
         <div className="flex items-center space-x-1">
           <div className="flex-1 border-b-[1px] border-[#4f535a]"></div>
         </div>
-        {/* コード */}
         <CopyTextField label="埋め込みコード :" text={code} />
-        {/* プレビュー */}
         <CodePreview code={code} />
       </div>
     </Modal>
