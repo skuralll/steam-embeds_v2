f3560eb5323741865b85c2ebf1612c0a981db385
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-16 13:33:22 +0900

    Update HomeFooter layout and add source code link

--- src/features/home/HomeFooter.tsx
+++ src/features/home/HomeFooter.tsx
@@ -1,14 +1,23 @@
 const HomeFooter = () => {
   return (
-    <footer className="bg-[#181d24]">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
-        <span className="block text-sm text-center text-gray-400">
-          © 2024{' '}
+    <footer className="bg-[#181d24] p-4">
+      <div className="w-full mx-auto max-w-4xl p-2 flex items-center justify-between">
+        <span className="text-sm text-gray-400">
+          © 2023{' '}
           <a href="https://github.com/skuralll" className="hover:underline">
             skuralll
           </a>
         </span>
+        <ul className="flex flex-wrap items-center text-sm font-medium text-gray-400">
+          <li>
+            <a
+              href="https://github.com/skuralll/steam-embeds_v2"
+              className="hover:underline me-4 md:me-6"
+            >
+              ソースコード
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
