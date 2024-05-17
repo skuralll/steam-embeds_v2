ceb7a12af46d480207ecf7da9be33d0caec5cf87
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-17 14:56:06 +0900

    Update copyright year to 2024 in HomeFooter component

--- src/features/home/HomeFooter.tsx
+++ src/features/home/HomeFooter.tsx
@@ -3,7 +3,7 @@ const HomeFooter = () => {
     <footer className="bg-[#181d24] p-4">
       <div className="w-full mx-auto max-w-4xl p-2 flex items-center justify-between">
         <span className="text-sm text-gray-400">
-          © 2023{' '}
+          © 2024{' '}
           <a href="https://github.com/skuralll" className="hover:underline">
             skuralll
           </a>
