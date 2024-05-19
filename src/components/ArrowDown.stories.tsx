770572ddbc080b4387d765a7658ec55bcd7b7c8f
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-19 19:56:31 +0900

    Add background parameter and update arrow fill color

--- src/components/ArrowDown.stories.tsx
+++ src/components/ArrowDown.stories.tsx
@@ -5,6 +5,11 @@ import ArrowDown from "./ArrowDown";
 const meta: Meta<typeof ArrowDown> = {
   title: "Common/ArrowDown",
   component: ArrowDown,
+  parameters: {
+    backgrounds: {
+      default: "steam",
+    },
+  },
   tags: ["autodocs"],
   args: {},
   argTypes: {},
--- src/components/ArrowDown.tsx
+++ src/components/ArrowDown.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ArrowDown = () => {
   return (
     <svg
-      fill="#000000"
+      fill="#ffffff"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 330 330"
       xmlSpace="preserve"
