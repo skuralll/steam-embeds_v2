58d6a7e40d26ea6b42690780279ebb738524ccba
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-19 17:16:10 +0900

    Update widget to display 10 items & add scroll detection

--- src/features/widget/GameCardList.tsx
+++ src/features/widget/GameCardList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { PlayedGameData } from '../../types/Game';
 import GameCard from './card/GameCard';
 
@@ -6,8 +7,34 @@ type Props = {
 };
 
 const GameCardList = ({ games }: Props) => {
+  // スクロール検知
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const [isBottom, setIsBottom] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = scrollRef.current;
+      if (element) {
+        if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+          setIsBottom(true);
+        } else {
+          setIsBottom(false);
+        }
+      }
+    };
+    const element = scrollRef.current;
+    if (element) {
+      element.addEventListener('scroll', handleScroll);
+    }
+    // クリーンアップ関数
+    return () => {
+      if (element) {
+        element.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+
   return (
-    <div className="w-full h-full bg-[#16202d] overflow-y-scroll">
+    <div ref={scrollRef} className="w-full h-full bg-[#16202d] overflow-y-scroll">
       <ol className="list-none">
         {games.map((game) => (
           <li key={game.appid} className="border-t-[2px] border-[#1b2838]">
